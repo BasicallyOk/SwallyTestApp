8abe6dfcc0ceb586ddb7b45a2c439b5855f641dc
AdamB4023 <99753574+AdamB4023@users.noreply.github.com>
2022-02-15 16:17:07 -0500

    Update housing.py

--- housing.py
+++ housing.py
@@ -1,3 +1,5 @@
+import requests
+
 self.swally_score = 0 #initializing swally points variable
 
 #if statments to keep count of a user's swally points
@@ -9,49 +11,68 @@ def swally_score_calculate(self, flag):
         self.swally_score -=100
         self.save()
 
-#if statments for the housing progression
+#if statments and api for the housing progression
+housing = {'first': '1', 'second': 2, 'third': 3,'fourth': 4, 'fifth': 5, 'sixth': 6, 'seventh': 7,'eighth': 8, 'ninth': 9, 'tenth': 10,'eleventh': 11, 'twelth': 12, 'thirteenth': 13, 'fourteenth': 14,'fifthteenth': 15}
+api_url = "https://jsonplaceholder.typicode.com/todos/10"
+
 if (self.swally_score < 200):
-    housing.post('https://httpbin.org/post', data = {'1'})
+    response = requests.post(api_url, housing = {'first': 1})
+    response = requests.get(api_url)
+    response.housing()
  
 if (self.swally_score >= 200) & (self.swally_score < 600):
-    housing.put('https://httpbin.org/put', data = {'2'})
+    response = requests.put(api_url, housing = {'second': 2})
+    response.housing()
     
 if (self.swally_score >= 600) & (self.swally_score < 1400):
-    housing.put('https://httpbin.org/put', data = {'3'})
+    response = requests.put(api_url, housing = {'third': 3})
+    response.housing()
     
 if (self.swally_score  >= 1400) & (self.swally_score < 2600):
-    housing.put('https://httpbin.org/put', data = {'4'})
+    response = requests.put(api_url, housing = {'fourth': 4})
+    response.housing()
     
 if (self.swally_score >= 2600) & (self.swally_score < 4300):
-    housing.put('https://httpbin.org/put', data = {'5'})
+    response = requests.put(api_url, housing = {'fifth': 5})
+    response.housing()
     
 if (self.swally_score  >= 4300) & (self.swally_score < 6500):
-    housing.put('https://httpbin.org/put', data = {'6'})
+    response = requests.put(api_url, data = {'sixth': 6})
+    response.housing()
     
 if (self.swally_score >= 6500) & (self.swally_score < 9300):
-    housing.put('https://httpbin.org/put', data = {'7'})
+    response = requests.put(api_url, data = {'seventh': 7})
+    response.housing()
     
 if (self.swally_score >= 9300) & (self.swally_score < 12700):
-    housing.put('https://httpbin.org/put', data = {'8'})
+    response = requests.put(api_url, data = {'eighth': 8})
+    response.housing()
     
 if (self.swally_score >= 12700) & (self.swally_score < 16700)
-    housing.put('https://httpbin.org/put', data = {'9'})
+    response = requests.put(api_url, data = {'ninth': 9})
+    response.housing()
     
 if (self.swally_score >= 16700) & (self.swally_score < 21400)
-    housing.put('https://httpbin.org/put', data = {'10'}
-    
+    response = requests.put(api_url, data = {'tenth': 10})
+    response.housing()
+                            
 if (self.swally_score >= 21400) & (self.swally_score < 26800):
-    housing.put('https://httpbin.org/put', data = {'11'})
-    
+    response = requests.put(api_url, data = {'eleventh': 11}
+    response.housing()
+                            
 if (self.swally_score >= 26800) & (self.swally_score < 32900)
-    housing.put('https://httpbin.org/put', data = {'12'})
-    
+    response = requests.put(api_url, data = {'twelth': 12})
+    response.housing()
+                            
 if (self.swally_score >= 32900) & (self.swally_score < 3800):
-    housing.put('https://httpbin.org/put', data = {'13'})
- 
+    response = requests.put(api_url, data = {'thirteenth': 13})
+    response.housing()
+                            
 if (SP >= 39800) & (SP < 47500):
-    housing.put('https://httpbin.org/put', data = {'14'})
-    
+    response = requests.put(api_url, data = {'fourteenth': 14})
+    response.housing()
+                            
 if (SP >= 47500):  
-    housing.put('https://httpbin.org/put', data = {'15'}
- 
+    response = requests.put(api_url, data = {'fifthteenth': 15'})
+    response.housing()
+                            
