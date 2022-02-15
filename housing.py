01dbcaa5987b4937c39e37cfeb8f7384d56fc9bf
AdamB4023 <99753574+AdamB4023@users.noreply.github.com>
2022-02-15 12:23:19 -0500

    Update housing.py

--- housing.py
+++ housing.py
@@ -1,1 +1,53 @@
-# Put code here
+self.swally_score = 0 #initializing swally points variable
+
+Housing = models.IntegerField(max_length = 15, null = True) # need help
+
+#if statments to keep count of a user's swally points
+def swally_score_calculate(self, flag):
+    if flag == 0:
+        self.swally_score +=100
+        self .save()
+    else:
+        self.swally_score -=100
+        self.save()
+
+#if statments for the housing progression
+if (self.swally_score < 200):
+    Display House_1 #NEED HELP
+ 
+if (self.swally_score >= 200) & (self.swally_score < 600):
+    Display House_2 #NEED HELP
+    
+if (self.swally_score >= 600) & (self.swally_score < 1400):
+    Display House_3 #NEED HELP
+    
+if (self.swally_score  >= 1400) & (self.swally_score < 2600):
+    Display House_4 #NEED HELP
+    
+if (self.swally_score >= 2600) & (self.swally_score < 4300):
+    Display House_5 #NEED HELP
+    
+if (self.swally_score  >= 4300) & (self.swally_score < 6500):
+    Display House_6 #NEED HELP
+    
+if (self.swally_score >= 6500) & (self.swally_score < 9300):
+    Display House_7 #NEED HELP
+    
+if (self.swally_score >= 9300) & (self.swally_score < 12700):
+    Display House_8 #NEED HELP
+    
+if (self.swally_score >= 12700) & (self.swally_score < 16700)
+    Display House_9 #NEED HELP
+    
+if (self.swally_score >= 16700) & (self.swally_score < 21400)
+    Display House_10 #NEED HELP
+    
+if (self.swally_score >= 21400) & (self.swally_score < 26800):
+    Display House_11 #NEED HELP
+    
+if (self.swally_score >= 26800) & (self.swally_score < 32900)
+    Display House_12 #NEED HELP
+    
+if (self.swally_score >= 32900) & (self.swally_score < 39800):
+    Display House_13 #NEED HELP
+ 
